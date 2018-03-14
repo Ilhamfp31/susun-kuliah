2370374a9c8e007e687e291d9d28aa4f159aaf97
yusufrahmatp <yusufrahmatp@gmail.com>
2018-03-14 12:11:03 +0700

    Add BFS algo in Graph.cs

--- susun-kuliah/susun-kuliah/Graph.cs
+++ susun-kuliah/susun-kuliah/Graph.cs
@@ -7,7 +7,6 @@ namespace susunkuliah
     public class Graph
     {
         Dictionary<string, List<string>> adj = new Dictionary<string, List<string>();
-        List<>
         int cur_timestamp = 0;
 
         public Graph()
@@ -28,7 +27,66 @@ namespace susunkuliah
 
         public TopologicalSortDFS() {
             cur_timestamp = 0;
-
         }
+
+        public List<List <string> > TopologicalSortBFS() {
+            Dictionary<string, int> counter = new Dictionary<string, int>();
+            List<string> deliminator = new List<string>();
+            List<List <string> > output = new List<List <string> >();
+            bool end = false;
+            bool continuity = true;
+
+            counter = fillCounter();
+
+            while (!end) && continuity {
+                end = true;
+                continuity = false;
+                foreach (var item in counter) {
+                    if (item.Value == 0) {
+                        continuity = true;
+                        deliminator.Add(item.Key);
+                    }
+                    else {
+                        end = false;
+                    }
+                }
+
+                if (!continuity) && (!end) {
+                    //ERROR
+                }
+                else {
+                    foreach (var vertice in deliminator) {
+                        if (counter.ContainsKey(vertice)) {
+                            foreach (var target in adj[vertice]) {
+                                counter[target]--;
+                            }
+                            counter.Remove(vertice);
+                        }
+                    }
+
+                    output.Add(new List<string> (deliminator));
+                }
+            }
+            return output;
+        }
+
+        private Dictionary<string, int> fillCounter() {
+            Dictionary<string, int> counter = new Dictionary<string, int>();
+            foreach (var item in adj) {
+                if (!counter.ContainsKey(item.Key)) {
+                    counter.Add(item.key, 0);
+                }
+                for (int i = 0; i < item.Value.Count; i++) {
+                    if counter.ContainsKey(item.Value[i]) {
+                        counter[item.Value[i]]++;
+                    }
+                    else {
+                        counter.Add(item.key, 1);
+                    }
+                }
+            }
+            return counter;          
+        }
+
     }
 }
